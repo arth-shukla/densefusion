41fa8550ec0b83fb9d776e861d5ab0449127d625
arth-shukla <44527268+arth-shukla@users.noreply.github.com>
2023-11-16 11:55:09 -0800

    Feat: Occlusion ds processing, DataLoader

--- process_raw_data_occlusions.py
+++ process_raw_data_occlusions.py
@@ -95,23 +95,6 @@ def process_raw_data(output_dir = 'processed_data'):
     def get_stripped_lines(fp, levels=[1, 2]):
         return [x.strip() for x in open(fp, 'r').readlines() if int(x[0]) in levels]
 
-    def get_ptcld_from_depth_using_mask(depth, mask, rgb, intrinsic, inv_extrinsic):
-        def apply_transformation(x, T):
-            return x @ T[:3, :3].T + T[:3, 3]
-
-        z = depth * mask
-        v, u = np.indices(z.shape)
-
-        uv1 = np.stack([u + 0.5, v + 0.5, np.ones_like(z)], axis=-1)
-        points_viewer = uv1 @ np.linalg.inv(intrinsic).T * z[..., None]  # [H, W, 3]
-
-        pv = points_viewer.reshape([-1, 3])
-        keep_pts = ~np.all(pv == 0, axis=1)
-
-        pts_rgb = rgb.reshape([-1, 3])[keep_pts]
-
-        return apply_transformation(pv[keep_pts], inv_extrinsic), pts_rgb
-
     def crop_img_using_mask(img, choose, mask, ret_masked=True):
         if ret_masked:
             img = img * mask[..., None]
@@ -120,8 +103,16 @@ def process_raw_data(output_dir = 'processed_data'):
     
     def process_raw_obs_data(raw_obj_dir, scene_names, output_dir, processed_models_dir, test=False, min_ptcld_len=1, max_ptcld_len=1000):
         dp_num = 0
+        scene_num = 0
 
         os.makedirs(output_dir, exist_ok=True)
+        scene_dir = output_dir / 'scene_masks'
+        rgb_dir = output_dir / 'rgbs'
+        depth_dir = output_dir / 'depths'
+        intrinsic_dir = output_dir / 'intrinsics'
+        inv_extrinsic_dir = output_dir / 'inv_extrinsics'
+        for dirname in [scene_dir, rgb_dir, depth_dir, intrinsic_dir, inv_extrinsic_dir]:
+            os.makedirs(dirname, exist_ok=True)
 
         for scene in tqdm(scene_names):
             color_path = raw_obj_dir / f'{scene}_color_kinect.png'
@@ -134,45 +125,31 @@ def process_raw_data(output_dir = 'processed_data'):
             label = np.array(Image.open(label_path))
             meta = pickle.load(open(meta_path, 'rb'))
 
+            scene_mask = np.zeros_like(label)
+            intrinsic = meta['intrinsic']
             inv_extrinsic = np.linalg.inv(meta['extrinsic'])
 
             for obj_id, obj_name in zip(meta['object_ids'], meta['object_names']):
                 mask = (label.copy() == obj_id)
+                scene_mask += mask
 
-                keep_ms, keep_ns = np.nonzero(mask > 0)
-                if len(keep_ms) > max_ptcld_len:
-                    keep_idxs = random.sample(list(range(len(keep_ms))), max_ptcld_len)
-                    keep_ms = keep_ms[keep_idxs]
-                    keep_ns = keep_ns[keep_idxs]
-                    choose = np.zeros_like(mask)
-                    choose[(keep_ms, keep_ns)] = 1
-                else:
-                    choose = mask.copy()
+                if np.all(mask == False):
+                    continue
                 
                 scale = np.array(meta['scales'][obj_id])
 
-                pts, pts_rgb = get_ptcld_from_depth_using_mask(depth, choose, color, meta['intrinsic'], inv_extrinsic)
-                if len(pts) < min_ptcld_len:
-                    continue
-
                 rmin, rmax, cmin, cmax = get_bbox(mask_to_bbox(mask))
-                color_masked = color.copy() * mask.reshape(*mask.shape, 1)
-                cropped_img = color_masked[rmin:rmax, cmin:cmax]
-                choose = choose[rmin:rmax, cmin:cmax]
-                # mask = mask[rmin:rmax, cmin:cmax]
-
                 model = np.array(o3d.io.read_point_cloud(str(processed_models_dir / f'{obj_name}.pcd')).points) * scale
                 new_meta = dict(
                     obj_id=obj_id,
                     obj_name=obj_name,
+                    rmin=rmin, rmax=rmax,
+                    cmin=cmin, cmax=cmax,
+                    scene_num=scene_num,
                 )
 
-                np.save(output_dir / f'{dp_num}_point_cloud', pts)
-                np.save(output_dir / f'{dp_num}_point_cloud_rgb', pts_rgb)
-                np.save(output_dir / f'{dp_num}_cropped_rgb', cropped_img)
-                # np.save(output_dir / f'{dp_num}_mask', mask)
                 np.save(output_dir / f'{dp_num}_model', model)
-                np.save(output_dir / f'{dp_num}_choose', choose)
+                np.save(output_dir / f'{dp_num}_mask', mask)
                 with open(output_dir / f'{dp_num}_meta.pkl', 'wb') as handle:
                     pickle.dump(new_meta, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -183,6 +160,14 @@ def process_raw_data(output_dir = 'processed_data'):
                     np.save(output_dir / f'{dp_num}_target', target)
 
                 dp_num += 1
+
+            scene_mask = scene_mask > 0
+            np.save(scene_dir / f'{scene_num}_scene_mask', scene_mask)
+            np.save(rgb_dir / f'{scene_num}_rgb', color)
+            np.save(depth_dir / f'{scene_num}_depth', depth)
+            np.save(intrinsic_dir / f'{scene_num}_intrinsic', intrinsic)
+            np.save(inv_extrinsic_dir / f'{scene_num}_inv_extrinsic', inv_extrinsic)
+            scene_num += 1
     
     raw_data_dir = Path('raw_data')
 
@@ -200,17 +185,17 @@ def process_raw_data(output_dir = 'processed_data'):
     processed_test_dir = processed_data_dir / 'test'
     processed_models_dir = processed_data_dir / 'models'
 
-    print('Farthest point sampling model pointclouds...')
-    os.makedirs(processed_models_dir, exist_ok=True)
-    for obj in tqdm(os.listdir(raw_models_dir)):
-        if str(obj) == '.gitignore': continue
-        model = trimesh.load(raw_models_dir / obj / 'visual_meshes/visual.dae', force='mesh')
-        pts, _ = trimesh.sample.sample_surface(model, 10000, seed=0)
-        pts = fp_sampling(pts, num_samples=1000)
+    # print('Farthest point sampling model pointclouds...')
+    # os.makedirs(processed_models_dir, exist_ok=True)
+    # for obj in tqdm(os.listdir(raw_models_dir)):
+    #     if str(obj) == '.gitignore': continue
+    #     model = trimesh.load(raw_models_dir / obj / 'visual_meshes/visual.dae', force='mesh')
+    #     pts, _ = trimesh.sample.sample_surface(model, 10000, seed=0)
+    #     pts = fp_sampling(pts, num_samples=1000)
 
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(pts)
-        o3d.io.write_point_cloud(str(processed_models_dir / f'{obj}.pcd'), pcd)
+    #     pcd = o3d.geometry.PointCloud()
+    #     pcd.points = o3d.utility.Vector3dVector(pts)
+    #     o3d.io.write_point_cloud(str(processed_models_dir / f'{obj}.pcd'), pcd)
 
     train_scene_names = get_stripped_lines(raw_train_splits_dir / 'train.txt')
     val_scene_names = get_stripped_lines(raw_train_splits_dir / 'val.txt')
@@ -224,4 +209,4 @@ def process_raw_data(output_dir = 'processed_data'):
     process_raw_obs_data(raw_train_obj_dir, train_scene_names, processed_train_dir, processed_models_dir)
 
 if __name__ == '__main__':
-    process_raw_data(output_dir='processed_like_df_2')
+    process_raw_data(output_dir='processed_occlusions')
